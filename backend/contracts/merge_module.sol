0fe41a31cdfd59cd9bbecdd3a92e18cea74460d3
Kyle Morris <globeofgeek@gmail.com>
2018-06-23 10:28:09 -0700

    Initialize a merge module.
    Fixes #2

---
+++ backend/contracts/merge_module.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.0;
+import "./module_interface.sol";
+
+contract MergeModule {
+    constructor(){
+    }
+
+    function execute() public {
+    }
+}
--- backend/contracts/test_contracts/dummy_asc.sol
+++ backend/contracts/test_contracts/dummy_asc.sol
@@ -1,13 +1,16 @@
 pragma solidity ^0.4.0;
 
 import "../ASC_interface.sol";
+import "../merge_module.sol";
 
 contract DummyASC is ASC_interface {
+    MergeModule public merge_module;
+
     constructor(){
+        merge_module = new MergeModule();
     }
 
     function execute() public {
         // TODO this should execute a module instead of do nothing
     }
-
 }
