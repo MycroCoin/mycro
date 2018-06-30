3c90dfe5cab7c7277f738f642f0a9230524441a2
Paymahn Moghadasian <paymahn1@gmail.com>
2018-06-29 18:19:53 -0700

    Merge PRs by id
    Event translator now has a function to merge pull requests by ID

--- backend/src/translator/event_translator.py
+++ backend/src/translator/event_translator.py
@@ -5,14 +5,20 @@ import os
 def get_access_token():
     return os.environ["GITHUB_ACCESS_TOKEN"]
 
+def merge_pull_request(github: Github, repo_name: str, pull_request_id: int):
+    repo = github.get_user().get_repo(repo_name)
+
+    desired_pull_request = repo.get_pull(pull_request_id)
+
+    desired_pull_request.merge()
+
 
 def main():
     token = get_access_token()
 
     github = Github(token)
 
-    for repo in github.get_user().get_repos():
-        print(repo.name)
+    merge_pull_request(github, 'lol-test', 1)
 
 
 if __name__ == "__main__":
