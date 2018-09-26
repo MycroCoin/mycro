2acccdfafdff115af89eab593d2cf4bdc90974ce
Paymahn Moghadasian <paymahn1@gmail.com>
2018-09-26 11:54:48 -0400

    Validate a request to create a merge ASC
    The server now performs validation before creating a merge asc.

--- backend/server/schemas/asc.py
+++ backend/server/schemas/asc.py
@@ -1,12 +1,17 @@
 import graphene
-
-from graphene_django.types import DjangoObjectType
 from graphene import ObjectType
+from graphene_django.types import DjangoObjectType
 
+import backend.server.utils.deploy as deploy
+import backend.server.utils.github as github
+import backend.settings as settings
 from backend.server.models import ASC, Project
 from backend.server.utils.contract_compiler import ContractCompiler
-import backend.server.utils.deploy as deploy
-import backend.settings as settings
+
+
+class ASCError(Exception):
+    """Dummy class for ASC problems"""
+    pass
 
 
 class AscType(DjangoObjectType):
@@ -17,7 +22,6 @@ class AscType(DjangoObjectType):
     voters = graphene.List(graphene.String)
 
     def resolve_has_executed(self: ASC, info):
-
         asc_contract = deploy.get_asc_contract(self.address)
 
         return asc_contract.functions.hasExecuted().call()
@@ -26,7 +30,6 @@ class AscType(DjangoObjectType):
         dao_contract = deploy.get_dao_contract(self.project.dao_address)
 
         return dao_contract.functions.getAscVotes(self.address).call()
-
 
 
 class Query(ObjectType):
@@ -54,8 +57,9 @@ class Query(ObjectType):
         if address is not None:
             return ASC.objects.get(address=address)
 
-        raise Exception("Uhoh, something went horribly wrong. This should never be raised. Graphql won't allow neither "
-                        "of these parameters to exist.")
+        raise Exception(
+            "Uhoh, something went horribly wrong. This should never be raised. Graphql won't allow neither "
+            "of these parameters to exist.")
 
     def resolve_asc_for_project(self, info, project_address):
         ascs = ASC.objects.filter(project__dao_address=project_address)
@@ -64,8 +68,10 @@ class Query(ObjectType):
     def resolve_get_merge_asc_abi(self, info):
         contract_compiler = ContractCompiler()
 
-        merge_asc_interface = contract_compiler.get_contract_interface("merge_asc.sol", "MergeASC")
-        return {'abi': merge_asc_interface['abi'], 'unlinked_binary': merge_asc_interface['bin']}
+        merge_asc_interface = contract_compiler.get_contract_interface(
+            "merge_asc.sol", "MergeASC")
+        return {'abi': merge_asc_interface['abi'],
+                'unlinked_binary': merge_asc_interface['bin']}
 
 
 class CreateMergeASC(graphene.Mutation):
@@ -77,33 +83,62 @@ class CreateMergeASC(graphene.Mutation):
 
     asc = graphene.Field(AscType)
 
-    def mutate(self, info, dao_address: str, rewardee: str, reward: int, pr_id: int):
+    def mutate(self, info, dao_address: str, rewardee: str, reward: int,
+               pr_id: int):
         # validate that we have a DAO with the given address in our DB
         # this may be unnecessary
         project = Project.objects.get(dao_address=dao_address)
 
         # TODO check that a PR with the given ID exists before executing the rest of this function
+        CreateMergeASC._validate_asc_creation(project, pr_id)
 
         compiler = ContractCompiler()
 
         w3 = deploy.get_w3()
-        base_dao_interface = compiler.get_contract_interface('base_dao.sol', 'BaseDao')
-        dao_contract = w3.eth.contract(abi=base_dao_interface['abi'], address=dao_address)
+        base_dao_interface = compiler.get_contract_interface('base_dao.sol',
+                                                             'BaseDao')
+        dao_contract = w3.eth.contract(abi=base_dao_interface['abi'],
+                                       address=dao_address)
 
-        asc_interface = compiler.get_contract_interface('merge_asc.sol', 'MergeASC')
+        asc_interface = compiler.get_contract_interface('merge_asc.sol',
+                                                        'MergeASC')
 
         # we don't use the async method here because we can't parallelize
         # first the asc has to be deployed to get it's address then the address has to be registered
         # with the base dao
-        _, _, asc_address, _ = deploy.deploy(asc_interface, rewardee, reward, pr_id, private_key=settings.ethereum_private_key())
+        _, _, asc_address, _ = deploy.deploy(asc_interface, rewardee, reward,
+                                             pr_id,
+                                             private_key=settings.ethereum_private_key())
 
-        deploy.call_contract_function(dao_contract.functions.propose, asc_address,
+        deploy.call_contract_function(dao_contract.functions.propose,
+                                      asc_address,
                                       private_key=settings.ethereum_private_key())
 
-        asc = ASC(address=asc_address, project=project, rewardee=rewardee, reward=reward, pr_id=pr_id)
-        asc.save()
+        asc = project.asc_set.create(address=asc_address, project=project,
+                                     rewardee=rewardee, reward=reward,
+                                     pr_id=pr_id)
 
         return CreateMergeASC(asc=asc)
+
+    @staticmethod
+    def _validate_asc_creation(project: Project, pr_id: int):
+        # first check to see if the project already has an ASC open or closed for this pr
+        for asc in project.asc_set.all():
+            if asc.pr_id == pr_id:
+                raise ASCError(
+                    f'ASC at address `{asc.address} already exists for pr {pr_id}')
+
+        # next make sure the project has an OPEN pr with this ID
+        for pull_request in github.get_pull_requests(project.repo_name,
+                                                     settings.github_organization(),
+                                                     settings.github_token(),
+                                                     state='open'):
+            if pull_request.number == pr_id:
+                # There's an open PR with this ID
+                return
+
+        raise ASCError(
+            f'Could not find an open PR with number {pr_id} on project {project.repo_name}')
 
 
 class Mutation(graphene.ObjectType):
--- backend/tests/schema/test_asc_schema.py
+++ backend/tests/schema/test_asc_schema.py
@@ -1,15 +1,20 @@
 from backend.server.models import Project, ASC
 from backend.tests.mycro_django_test import MycroDjangoTest
 import backend.tests.testing_utilities.constants as constants
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 
 class TestASCSchema(MycroDjangoTest):
 
     def setUp(self):
         super().setUp()
-        self.project = Project.objects.create(repo_name=constants.PROJECT_NAME, dao_address=constants.DAO_ADDRESS)
-        self.asc = ASC.objects.create(address=constants.ASC_ADDRESS, project=self.project, reward=constants.REWARD)
+        self.project = Project.objects.create(repo_name=constants.PROJECT_NAME,
+                                              dao_address=constants.DAO_ADDRESS)
+        self.asc = self.project.asc_set.create(address=constants.ASC_ADDRESS,
+                                      project=self.project,
+                                      reward=constants.REWARD,
+                                      pr_id=constants.PR_ID)
+
 
     def test_get_all_ascs(self):
         resp = self.query(
@@ -21,7 +26,8 @@ query {
 }
             '''
         )
-        self.assertResponseNoErrors(resp, {'allAscs': [{'address': constants.ASC_ADDRESS}]})
+        self.assertResponseNoErrors(resp, {
+            'allAscs': [{'address': constants.ASC_ADDRESS}]})
 
     def test_get_asc_by_project_address(self):
         resp = self.query(
@@ -36,7 +42,8 @@ query {{
             '''
         )
 
-        self.assertResponseNoErrors(resp, {'ascForProject': [{'project': {'repoName': constants.PROJECT_NAME}}]})
+        self.assertResponseNoErrors(resp, {'ascForProject': [
+            {'project': {'repoName': constants.PROJECT_NAME}}]})
 
     def test_get_asc_by_id(self):
         resp = self.query(
@@ -49,7 +56,8 @@ query {
             '''
         )
 
-        self.assertResponseNoErrors(resp, {'asc': {'address': constants.ASC_ADDRESS}})
+        self.assertResponseNoErrors(resp,
+                                    {'asc': {'address': constants.ASC_ADDRESS}})
 
     def test_get_asc_by_address(self):
         resp = self.query(
@@ -79,11 +87,13 @@ mutation {{
         self.assertErrorNoResponse(resp, "Project matching query")
 
     @patch('backend.server.utils.deploy.get_w3')
-    def test_create_asc(self, get_w3_mock):
+    @patch('backend.server.schemas.asc.CreateMergeASC._validate_asc_creation')
+    def test_create_asc(self, validate_mock, get_w3_mock):
         ASC.objects.filter().delete()
+        self.assertEqual(0, self.project.asc_set.count())
         w3 = get_w3_mock.return_value
-        w3.eth.waitForTransactionReceipt.return_value = {'contractAddress': constants.ASC_ADDRESS}
-
+        w3.eth.waitForTransactionReceipt.return_value = {
+            'contractAddress': constants.ASC_ADDRESS}
 
         # need to double up on braces because of f-strings
         resp = self.query(f"""
@@ -95,7 +105,8 @@ mutation {{
   }}
 }}
 """)
-        self.assertResponseNoErrors(resp, {'createMergeAsc': {'asc': {'address': constants.ASC_ADDRESS}}})
+        self.assertResponseNoErrors(resp, {
+            'createMergeAsc': {'asc': {'address': constants.ASC_ADDRESS}}})
 
         all_ascs = ASC.objects.all()
 
@@ -104,9 +115,12 @@ mutation {{
         self.assertEqual(constants.ASC_ADDRESS, all_ascs[0].address)
 
         # called once for deployment and once for registration
-        self.assertEqual(2, get_w3_mock.return_value.eth.waitForTransactionReceipt.call_count)
+        self.assertEqual(2,
+                         get_w3_mock.return_value.eth.waitForTransactionReceipt.call_count)
         self.assertEqual(2, w3.eth.sendRawTransaction.call_count)
 
+        self.assertEqual(1, self.project.asc_set.count())
+        validate_mock.assert_called_once()
 
     def test_get_merge_asc_abi(self):
         resp = self.query("""
@@ -118,3 +132,40 @@ query {
         # basically just testing that nothing errors out
         # don't want to assert anything on the returned abi because that will make this test brittle
         self.assertNotIn('errors', resp)
+
+    def test_create_asc_already_exists(self):
+        resp = self.query(
+            f"""
+            mutation {{
+              createMergeAsc(daoAddress: "{constants.DAO_ADDRESS}", rewardee: "{constants.REWARDEE}", reward: {constants.REWARD},  prId: {constants.PR_ID})  {{
+                asc {{
+                  address 
+                }}
+              }}
+            }}
+            """)
+
+        self.assertErrorNoResponse(resp, 'already exists')
+
+    @patch('backend.server.schemas.asc.github')
+    @patch.dict('backend.settings.os.environ', {'GITHUB_TOKEN': constants.GITHUB_ACCESS_TOKEN, 'GITHUB_ORGANIZATION': constants.GITHUB_ORGANIZATION})
+    def test_create_asc_no_open_pr(self, github_mock):
+        pr_id = constants.PR_ID + 1
+
+        open_pr = MagicMock()
+        open_pr.number = pr_id + 1
+        github_mock.get_pull_requests.return_value = [open_pr]
+
+        resp = self.query(
+            f"""
+            mutation {{
+              createMergeAsc(daoAddress: "{constants.DAO_ADDRESS}", rewardee: "{constants.REWARDEE}", reward: {constants.REWARD},  prId: {pr_id})  {{
+                asc {{
+                  address 
+                }}
+              }}
+            }}
+            """)
+
+        self.assertErrorNoResponse(resp, 'open PR with')
+        github_mock.get_pull_requests.assert_called_once_with(self.project.repo_name, constants.GITHUB_ORGANIZATION, constants.GITHUB_ACCESS_TOKEN, state='open')
