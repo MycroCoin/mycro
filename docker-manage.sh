29bf7c7b0ed23aa8a08bb760d4b6edb7e4d8a421
Aaron Peddle <aaronpeddle@aarons-mbp.lan>
2018-10-16 20:09:19 -0700

    add helper script for using manage.py from mycro-backend docker context

---
+++ docker-manage.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# wrapper to make callng manage.py helper functions within 
+# mycro-backend docker context easier.
+# example usage: "./docker-manage.sh makemigrations"
+
+docker run -v `pwd`:`pwd` -w `pwd` -i -t mycro-backend ./manage.py "$@"
+
